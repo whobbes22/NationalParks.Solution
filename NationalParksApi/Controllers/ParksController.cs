a805284255ddb0c56b14b5f12a659ef4945e97b6
Joe W <whobbes22@gmail.com>
2023-06-09 11:04:47 -0700

    add endpoint for searching by parameters

--- NationalParksApi/Controllers/ParksController.cs
+++ NationalParksApi/Controllers/ParksController.cs
@@ -16,6 +16,39 @@ namespace ParksAPI.Controllers
       _db = db;
     }
 
+    // GET api/parks
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Park>>> Get(string parkName, string parkType, string parkLocation, string parkSize, string parkDescription)
+    {
+      IQueryable<Park> query = _db.Parks.AsQueryable();
+      if (parkName != null)
+      {
+        query = query.Where(entry => entry.ParkName == parkName);
+      }
+
+      if (parkType != null)
+      {
+        query = query.Where(entry => entry.ParkType == parkType);
+      }
+
+      if (parkLocation != null)
+      {
+        query = query.Where(entry => entry.ParkLocation == parkLocation);
+      }
+
+      if (parkSize != null)
+      {
+        query = query.Where(entry => entry.ParkSize == parkSize);
+      }
+
+      if (parkDescription != null)
+      {
+        query = query.Where(entry => entry.ParkDescription == parkDescription);
+      }
+      return await query.ToListAsync();
+    }
+
+
     // GET: api/Reviews/5
     [HttpGet("{id}")]
     public async Task<ActionResult<Park>> GetReview(int id)
